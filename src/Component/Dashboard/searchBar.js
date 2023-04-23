117babd4f422a9a694214d5f0cc3f4647f24460e
Blessing Chukwubuogum <59502310+bleckcorp@users.noreply.github.com>
2023-04-23 21:13:56 +0100

    commit

--- src/Component/Dashboard/searchBar.js
+++ src/Component/Dashboard/searchBar.js
@@ -9,10 +9,10 @@ import Emoji from './Emoji.svg'
 // const fullName = decodedToken.fullName;
 
 const search = () => {
-    // const username = localStorage.getItem("fullName");
+    const username = localStorage.getItem("fullName");
     return <div className="search-menu">
         <div className="dashboard-content">
-            <h2>Welcome {"fullName"} <img src={Emoji} alt="Hive Emoji" /></h2>
+            <h2>Welcome {username.split(' ')[0]} <img src={Emoji} alt="Hive Emoji" /></h2>
             <div className="dashboard-search-component">
                 <h2>New Job Listing</h2>
                 <input type="text" placeholder="Search for a user" />
